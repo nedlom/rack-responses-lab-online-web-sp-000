7a03a63c01e639e6b4b6f381a893900c83a641dc
imperative-scientist-4600 <nelom3@gmail.com>
2021-03-13 21:17:43 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -3,5 +3,11 @@ class Application
   resp = Rack::Response.new
   
   if Time.new.hour < 12
+    resp.write "Good Morning!"
+  else
+    resp.write "Good Afternoon"
+  end
+  
+  resp.finish
     
 end
