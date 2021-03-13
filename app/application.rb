d58e8f8bf31dc04f5c0e6893c7bebc99721fb0b4
imperative-scientist-4600 <nelom3@gmail.com>
2021-03-13 21:19:08 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -6,7 +6,7 @@ class Application
     if Time.new.hour < 12
       resp.write "Good Morning!"
     else
-      resp.write "Good Afternoon"
+      resp.write "Good Afternoon!"
     end
     
     resp.finish
